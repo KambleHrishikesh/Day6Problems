d7accb6d71dabb1788c0806227008071fa52c6cd
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-06 22:36:46 +0530

    push prime number

---
+++ PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    public class PrimeNumber
+    {
+        public static void FindThePrimeNumber() 
+        {
+            int count = 0;
+            Console.WriteLine("Enter Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= (number / 2); i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 1)
+            {
+                Console.WriteLine(" Is Prime number");
+            }
+            else
+            {
+                Console.WriteLine(" Is not Prime number");
+            }
+        }
+    }
+}
