5b2c2a866484219cf73baff15a4fd824fa5a67d2
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-06 21:52:53 +0530

    master branch push

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Logical_Programs
+{
+     class Program 
+    {
+        public static void Main(string[] args)
+        {
+            //Fibonacci.PrintingFibonacci();
+            //PerfectNumber.FindingThePerfectNumber();
+            //ReverseNumber.ReverseTheNumber();
+            //PrimeNumber.FindThePrimeNumber();
+            //CouponNumber.Coupon();
+            //StopWatch.watch();
+            //VendingMachine.Notes();
+            //DayOfWeek.FindDay();
+            //TemperatureConversion.Temperature();
+            //MonthlyPayment.Payment();
+            //SquareRoot.Sqrt();
+            ToBinary.Binary();
+        }
+    }
+}
