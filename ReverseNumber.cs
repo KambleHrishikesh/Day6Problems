208a584d951617fd74953b74771abc0e1ba12663
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-06 22:19:16 +0530

    push reverse number

---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    public class ReverseNumber
+    {
+        public static void ReverseTheNumber()
+        {
+            int n, reverse = 0, rem = 0;
+            Console.WriteLine("Enter a Number ");
+            n=Convert.ToInt32(Console.ReadLine());  
+
+            while(n!=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n/=10;
+            }
+            Console.WriteLine("Reversed Number: " + reverse);
+        }
+    }
+}
