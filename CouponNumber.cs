ef116d1a1b2e5f3abbf75bf266776c24ebbfbc4a
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-06 22:45:30 +0530

    push coupon number

---
+++ CouponNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    public class CouponNumber
+    {
+        public int N;
+        public static void Coupon(int N)
+        {
+            Console.WriteLine("Enter N Value");
+            CouponNumber c = new CouponNumber();
+            N = Convert.ToInt32(Console.ReadLine());
+            CouponNumber.Coupon(N);
+
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(random.Next());
+            }
+        }
+    }
+}
